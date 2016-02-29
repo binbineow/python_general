c911e03c42a68ac9a6904c8ad9132d3ffa86d4db
binbineow <binbineow@gmail.com>
2016-02-29 04:32:55 -0800

    new RNN trainer save function

--- scripts_sh_verified/HLA_RNN_classifier_spar_v10.py
+++ scripts_sh_verified/HLA_RNN_classifier_spar_v10.py
@@ -49,6 +49,29 @@ def encoding(matrix0,input0, ctable0,len0):
         matrix0[i] = ctable0.encode(sentence, maxlen=len0)
     return matrix0
 
+def output_perf(file_out, file_name0, iteraions,train_pre,train_recall,val_pre,val_recall):
+    file_out.write(file_name+'_'+'iterations'+'\t')
+    for x0 in iterations:
+        file_out.write(x0+'\t')
+    file_out.write('n')
+    file_out.write(file_name+'_'+'Training_precision'+'\t')
+    for x0 in train_pre:
+        file_out.write(x0+'\t')
+    file_out.write('n')
+    file_out.write(file_name+'_'+'Training_recall'+'\t')
+    for x0 in train_recall:
+        file_out.write(x0+'\t')
+    file_out.write('n')
+    file_out.write(file_name+'_'+'Validation_precision'+'\t')
+    for x0 in val_pre:
+        file_out.write(x0+'\t')
+    file_out.write('n')
+    file_out.write(file_name+'_'+'Validation_recall'+'\t')
+    for x0 in val_recall:
+        file_out.write(x0+'\t')
+    file_out.write('n')
+    file_out.close()
+
 # class colors:
 #     ok = '\033[92m'
 #     fail = '\033[91m'
@@ -60,6 +83,8 @@ char_set = set([' '])
 class_set = set()
 max_len = 0
 X_train = []
+X_train_p = []
+X_train_n = []
 X_val_p = []
 X_val_n = []
 y_train = []
@@ -72,7 +97,12 @@ for line in fileinput.input(path_save+file_name0):
     if len(out_) != 1:
         raise Exception("Output should be single characer")
     else:
-        if out_ == '0' or out_ == '1':
+        if out_ == '0' :
+            X_train_n.append(in_)
+            X_train.append(in_)
+            y_train.append(out_)
+        elif out_ == '1':
+            X_train_p.append(in_)
             X_train.append(in_)
             y_train.append(out_)
         else:
@@ -90,7 +120,7 @@ for line in fileinput.input(path_save+file_name0):
         
         for c in in_: char_set.add(c)
         class_set.add(out_)
-file_name0 = filename.split('.')[0]      
+file_name0 = file_name.split('.')[0]      
 # Parameters for the model and dataset
 #TRAINING_SIZE = len(inputs)
 # Try replacing JZS1 with LSTM, GRU, or SimpleRNN
@@ -116,11 +146,15 @@ classtable = CharacterTable(classes, 1)
 X_train_m = np.zeros((len(X_train), MAXLEN, len(chars)), dtype=np.bool)
 X_val_p_m = np.zeros((len(X_val_p), MAXLEN, len(chars)), dtype=np.bool)
 X_val_n_m = np.zeros((len(X_val_n), MAXLEN, len(chars)), dtype=np.bool)
+X_train_p_m = np.zeros((len(X_train_p), MAXLEN, len(chars)), dtype=np.bool)
+X_train_n_m = np.zeros((len(X_train_n), MAXLEN, len(chars)), dtype=np.bool)
 y_train_m = np.zeros((len(y_train), len(classes)), dtype=np.bool)
 y_val_p_m = np.zeros((len(y_val_p), len(classes)), dtype=np.bool)
 y_val_n_m = np.zeros((len(y_val_n), len(classes)), dtype=np.bool)
 
 X_train = encoding(X_train_m, X_train,ctable,MAXLEN)
+X_train_p = encoding(X_train_p_m, X_train,ctable,MAXLEN)
+X_train_n = encoding(X_train_n_m, X_train,ctable,MAXLEN)
 X_val_p = encoding(X_val_p_m, X_val_p,ctable,MAXLEN)
 X_val_n = encoding(X_val_n_m, X_val_n,ctable,MAXLEN)
 y_train = encoding(y_train_m, y_train,classtable,1)
@@ -150,24 +184,49 @@ open(path_save+file_name0+'_model.json', 'w').write(json_string)
 #Create checkpoint
 checkpointer = ModelCheckpoint(filepath=model_name+'.weight', verbose=1, save_best_only=True)
 # Train the model each generation and show predictions against the validation dataset
+file_out = open(path_save+'model_performance_simplev1.csv')
+iterations = []
+train_pre = []
+train_recall = []
+val_pre = []
+val_recall = []
 for iteration in range(1, n_iteration):
+    iterations.append(iteration)
     print()
     print('-' * 50)
     print('Iteration', iteration)
     #to save weight callbacks=[checkpointer]
     model.fit(X_train, y_train, batch_size=BATCH_SIZE, nb_epoch=1, class_weight={1:1,0:1.0/ratio_t/2},validation_data=(X_val, y_val),show_accuracy=True)
-    #pickle(model,open(model_name+'.model'+n_version,'w+'))
-    print('Postive')
-    print(model.predict_classes(X_val_p)) 
+    ptotal0 = len(X_train_p)
+    ntotal0 = len(X_train_n)
+    #print('Train_Postive')
+    #print(model.predict_classes(X_val_p)) 
+    tp0 = sum(model.predict_classes(X_train_p))
+    #print('Train_Negative')
+    #print(model.predict_classes(X_val_n)) 
+    fp0 = sum(model.predict_classes(X_train_n))
+    tn0 = ntotal0 - fp0
+    fn0 = ptotal0 - tp0
+    train_pre.append(str(float(tp0)/(tp0+fp0)))
+    train_recall.append(str(float(tp0)/(tp0+fn0)))
+    print('Train_Precision='+str(float(tp0)/(tp0+fp0)))
+    print('Train_Recall='+str(float(tp0)/(tp0+fn0)))
+    
+    ######predicting validation
+    #print('Val_Postive')
+    #print(model.predict_classes(X_val_p)) 
     ptotal0 = len(X_val_p)
     ntotal0 = len(X_val_n)
     tp0 = sum(model.predict_classes(X_val_p))
-    print('Negative')
-    print(model.predict_classes(X_val_n)) 
+    #print('Val_Negative')
+    #print(model.predict_classes(X_val_n)) 
     fp0 = sum(model.predict_classes(X_val_n))
     tn0 = ntotal0 - fp0
     fn0 = ptotal0 - tp0
-    print('Precision='+str(float(tp0)/(tp0+fp0)))
-    print('Recall='+str(float(tp0)/(tp0+fn0)))
-
+    val_pre.append(str(float(tp0)/(tp0+fp0)))
+    val_recall.append(str(float(tp0)/(tp0+fn0)))
+    print('Train_Precision='+str(float(tp0)/(tp0+fp0)))
+    print('Train_Recall='+str(float(tp0)/(tp0+fn0)))
+#save weights and performance info
+output_perf(file_out,file_name0,iteraions,train_pre,train_recall,val_pre,val_recall)
 model.save_weights(path_save+file_name0+'_weight.h5')
