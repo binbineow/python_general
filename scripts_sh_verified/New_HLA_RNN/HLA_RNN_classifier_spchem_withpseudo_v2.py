a3db3bf44359655eb6d353153a6765f63e615350
binbineow <binbineow@gmail.com>
2016-08-11 23:12:10 -0400

    old

--- scripts_sh_verified/New_HLA_RNN/HLA_RNN_classifier_spchem_withpseudo_v2.py
+++ scripts_sh_verified/New_HLA_RNN/HLA_RNN_classifier_spchem_withpseudo_v2.py
@@ -44,7 +44,9 @@ for line0 in fileinput.input():
     line0 = line0.rstrip()
     #ideally add a line to remove spaces in each line
     part1 = line0.split('=')[0]
+    print(part1)
     part2 = line0.split('=')[1]
+    print(part2)
     if 'path_data' in part1:
         path_data = part2
     if 'path_save' in part1:
