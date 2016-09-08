919bb5080c771c59aea87d5f38250abea36449a6
binbineow <binbineow@gmail.com>
2016-09-08 10:47:40 -0700

    netmhc

--- scripts_sh_verified/New_HLA_RNN/predict_netmhciipan.py
+++ scripts_sh_verified/New_HLA_RNN/predict_netmhciipan.py
@@ -59,9 +59,9 @@ def run_netmhciipan(hla_type_run,list_run,len_run):
     print cmd_line
     cmd_line_list = cmd_line.split(' ')
     print cmd_line_list
-    subprocess.call(cmd_line_list)
-    #cmd0 = subprocess.Popen(cmd_line,shell=True)      
-    #cmd0.wait() 
+    #subprocess.call(cmd_line_list)
+    cmd0 = subprocess.Popen(cmd_line,shell=False)      
+    cmd0.wait() 
     #get data
     dict_out = read_netmhc_xls(file_name_in+'.xls', list_run)
     #remove temp file
