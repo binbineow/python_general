75cfe6accfa371a8178c7de0eeccd850179f0a34
binbineow <binbineow@gmail.com>
2016-09-28 13:17:04 -0700

    rnn_netmhcdata_predict_mcl

--- scripts_sh_verified/New_HLA_RNN/RNN_netmhcdata_on_MCL.py
+++ scripts_sh_verified/New_HLA_RNN/RNN_netmhcdata_on_MCL.py
@@ -69,6 +69,8 @@ def encoding_line(str0, max_len):
 
 def encoding(matrix0, input0, len0):
     for i, sentence in enumerate(input0):
+        if 'o' in stence:
+            print(sentence)
         matrix0[i] = encoding_line(sentence, len0)
     return matrix0
 
