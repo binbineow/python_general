da3bfab940ade4590ac412794fd876c222e1316e
binbineow <binbineow@gmail.com>
2016-09-28 13:38:51 -0700

    rnn_netmhcdata_predict_mcl

--- scripts_sh_verified/New_HLA_RNN/RNN_netmhcdata_on_MCL.py
+++ scripts_sh_verified/New_HLA_RNN/RNN_netmhcdata_on_MCL.py
@@ -89,9 +89,10 @@ def import_model(path_model,model_name0,weight_name0):
 def cal_percentile_from2lists(list1,list2,distr1,distr2):
     list_out = []
     for n0 in range(0,len(list1)):
-        val1 = percentileofscore(np.array(distr1), list1[n0])
-        val1 = percentileofscore(distr1, list1[n0])
-        print(val1)
+        #val1 = percentileofscore(np.array(distr1), list1[n0])
+        #val1 = percentileofscore(distr1, list1[n0])
+        #print(val1)
+        print(list1[n0])
         max0 = max(percentileofscore(distr1, list1[n0]),percentileofscore(distr2, list2[n0]))
         list_out.append(max0)
     return list_out
