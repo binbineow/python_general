1b5c5c345ca5b0031ab4f21ed77e6d6c50a6f425
binbineow <binbineow@gmail.com>
2016-10-10 22:07:40 -0700

    variable region test set

--- scripts_sh_verified/Ig_specific/RNN_on_MCL_igv.py
+++ scripts_sh_verified/Ig_specific/RNN_on_MCL_igv.py
@@ -33,7 +33,7 @@ mhc_dict_file = 'DRB1_pseudo_seq.dict'
 #path where the model is saved
 path_model = '/home/stanford/rbaltman/users/bchen45/results/HLA_pred_general_model/'
 model_name0 = 'hla_ii_train_val_nonIG_v1.txtnon_ig_v1_BSonly_n64_layer1_d0.3_l2_0.1_hnn0_model.json'
-weight_name0 = 'perf_non_ig_nonIG_v1non_ig_v1_s+b_only_n64_layer1_d0.3_l2_0.01.txt'
+weight_name0 = 'hla_ii_train_val_nonIG_v1.txtnon_ig_v1_s+b_only_n64_layer1_d0.3_l2_0.01_weight.h5'
 #sb model
 #model_name0 = 'hla_ii_train_val_nonIG_v3.txt_model.json'
 #weight_name0 = 'hla_ii_train_val_nonIG_v1.txtnon_ig_v1_s+b_only_n64_layer1_d0.3_l2_0.01_weight.h5'
