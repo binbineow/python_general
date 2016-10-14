b2d1a20db7e6b51f82ae326bd9e08ac061b21f0f
binbineow <binbineow@gmail.com>
2016-10-14 16:23:16 -0700

    predict

--- scripts_sh_verified/Ig_specific/plot_predictedv2.py
+++ scripts_sh_verified/Ig_specific/plot_predictedv2.py
@@ -4,5 +4,8 @@ def plot_2_lines(y_pred,y_reco,name0,path0='/home/stanford/rbaltman/users/bchen4
     #example name0 = MCL001_H_chain
     len0 = len(y_pred) #len y1 = y2
     x = np.linspace(1, len0+1, len0+1)
+    print(len(x))
+    print(len(y_pred))
+    print(len(y_reco))
     plt.fill(x,y_pred,'b',x,y_reco,'r',alpha=0.3)
     plt.savefig(path0+name0+'_pred_vs_recovered.png', bbox_inches='tight')
