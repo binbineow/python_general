0584086cd42efc620e24852266b95d117af29574
binbineow <binbineow@gmail.com>
2016-10-14 17:37:08 -0700

    predict

--- scripts_sh_verified/Ig_specific/plot_predictedv2.py
+++ scripts_sh_verified/Ig_specific/plot_predictedv2.py
@@ -6,16 +6,16 @@ def plot_2_lines(y_pred,y_reco,name0,path0='/home/stanford/rbaltman/users/bchen4
     #example name0 = MCL001_H_chain
 
     len0 = len(y_pred) #len y1 = y2
-    x = np.linspace(1, len0, len0)
+    x = np.linspace(0, len0, len0+1)
     #print(x)
-    y_pred = np.array(y_pred)
+    y_pred = np.array([0]+y_pred)
     #print(y_pred)
-    y_reco = np.array(y_reco)
+    y_reco = np.array([0]+y_reco)
     #print(y_reco)
     ax = plt.gca()
     plt.fill(x,y_pred,'b*',x,y_reco,'r*',alpha=0.2)
     ax.set_xlabel(name0+' Amino Acid Sequence')
     ax.set_ylabel('Peptides recovered/Predicted scores')
     ax.set_title(name0+' Peptide Recovered vs. MARIA Prediction Scores')
-    plt.savefig(path0+name0+'_pred_vs_recovered.png', bbox_inches='tight')
+    plt.savefig(path0+chain0+'/'+name0+'_pred_vs_recovered.png', bbox_inches='tight')
     plt.close()
