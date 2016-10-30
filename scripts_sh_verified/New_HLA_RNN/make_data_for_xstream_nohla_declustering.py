e070f38770118a51a06f6dbb4f116d1a781dbaab
binbineow <binbineow@gmail.com>
2016-10-30 14:21:25 -0700

    no shuffle

--- scripts_sh_verified/New_HLA_RNN/make_data_for_xstream_nohla_declustering.py
+++ scripts_sh_verified/New_HLA_RNN/make_data_for_xstream_nohla_declustering.py
@@ -145,8 +145,10 @@ for pid0 in pid_list:
 print('redundant set of peptide of '+mhc0+' ='+str(mhc_total))
 #make it unique
 list_pep_total = list(set(list_pep_total))
-print(list_pep_total[0:15])
+#print(list_pep_total[0:15])
 list_pep_total = limit_length(list_pep_total,9)
+print('After filtering, peptide n='+str(len(list_pep_total)))
+print(len(list_pep_total))
 list_clusters = get_cluster_list(list_pep_total)
 [list_train,list_val] = split_cluster(list_clusters,len(list_pep_total),split0)
 #0 indicates the code for negative data, 0 for training, 2 for validation
