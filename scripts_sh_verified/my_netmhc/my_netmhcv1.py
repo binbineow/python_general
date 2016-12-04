55beacc91e3067ca067671ded14fcd7001c4c64d
binbineow <binbineow@gmail.com>
2016-12-03 21:41:23 -0800

    leven

--- scripts_sh_verified/my_netmhc/my_netmhcv1.py
+++ scripts_sh_verified/my_netmhc/my_netmhcv1.py
@@ -14,12 +14,13 @@
 
 #####################import#################################
 from __future__ import print_function
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers.core import Activation, Masking, Dropout, Dense, RepeatVector
 from keras.layers import recurrent
 from keras.callbacks import ModelCheckpoint
 from utilities import *
 from keras.models import model_from_json
+from sklearn.metrics import roc_auc_score
 #from keras.regularizers import l1,activity_l1
 
 ######Path for data as well as performance output are read in from fileinput ###
@@ -47,14 +48,14 @@ path_dict = '/home/stanford/rbaltman/users/bchen45/code/python_general/encoding_
 #Blosum50_sparse.dict
 #Blosum50_only.dict
 #Sparse_only.dict
-dict_name = 'protvec.dict'
+dict_name = 'Blosum50_only.dict'
 b_shuffle = True
 loss_function0 = 'categorical_crossentropy'
 vb0 = 0
 nb = 3
-n_iteration = 30
-ratio_t = 1
-help_nn = 10
+n_iteration = 100
+ratio_t = 0.5
+help_nn = 60
 #input file path and parameters from the setting file
 for line0 in fileinput.input():
     line0 = line0.rstrip()
@@ -102,9 +103,13 @@ for line0 in fileinput.input():
         
  
 
-dict_aa = pickle.load(open(path_dict+dict_name,'r'))
-###determine the encoding size
-chars = dict_aa['AAA']
+
+# load blossom chemical encoding dictionary
+chem = pickle.load(open(open(path_dict+dict_name,'r')))
+# set '-' to all zeros
+chem['-'] = np.zeros(len(chem['A']))
+
+
    
 ##########################construct input file name####################  
 file_name0 = data_file_name+v1+'.txt'
@@ -143,11 +148,9 @@ classes = [0,1]
 
 ##########################start a model
 model = Sequential()
-# "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE
-#model.add(Masking())
-#print(str(LAYERS))
 model.add(Dense(help_nn))
-model.add(Activation('tanh'))
+model.add(Activation('relu'))
+classes = [0,1]
 model.add(Dense(len(classes)))
 model.add(Activation('softmax'))
 model.compile(loss=loss_function0, optimizer='adam')
@@ -157,31 +160,24 @@ model.compile(loss=loss_function0, optimizer='adam')
 
 #encoding will take a string or char, string=sequence and to return a matrix of encoded peptide sequence
 #char = class, '0' = non-binding (0,1), '1' = binding (1,0)
-def encoding_line(str0, max_len):
-    #print(type(dict_aa['A']))
-    #print(type(list(dict_aa['A'])))
-    #print(type(max_len))
-    if len(str0) == 1:
-        coded0 = np.zeros(2)
-        if str0 == '0':
-            coded0[0] = 1
-        else:
-            coded0[1] = 1
-    else:
-        coded0 = np.zeros((max_len,len(list(dict_aa['AAA']))))
-        for i in range(0,len(str0)-2):
-            if str0[i:i+3] in dict_aa:
-                coded0[i,:] = dict_aa[str0[i:i+3]]
-            else:
-                print('Error for encoding, '+str0[i:i+3])
-    #print(str0)
-    #print(coded0)
-    return coded0
 
-def encoding(matrix0, input0, len0):
-    for i, sentence in enumerate(input0):
-        matrix0[i] = encoding_line(sentence, len0)
-    return matrix0
+# encode string of peptides with chemical information
+def encode_seq(seq):
+  return np.array([chem[k] for k in seq]).flatten()
+
+            
+# generate sliding windows over core for netmhc technique
+# input: string of peptides
+# output: yield each netmhc vector encoding
+def make_windows(seq):
+  first = seq[0]
+  last = seq[-1]
+  for i in range(0,len(seq)-9+1):
+    core = seq[i:i+9]
+    num_begin = i
+    num_end = len(seq)-(i+9)
+    nums = np.array([num_begin, 1 - num_begin, num_end, 1 - num_end, len(seq), 1-len(seq)])
+    yield np.concatenate((encode_seq(first+core+last),nums))
 
 def output_perf2(list0):
     touch_file(path_save+performance_file_name+'.txt')     
@@ -209,9 +205,45 @@ def calf1(str1,str2):
     f1_out = 2.0*pre0*recall0/(pre0+recall0)
     return str(f1_out)
 
+# select strongest binding core for each example
+# input: x data matrix, y data, tracking info
+# output: strongest bindings for each x datapoint, corresponding y, corresponding outputs
+def select_best(x,y,track):
+  X_new, y_new, outs_new = [], [], []
+  outs = model.predict(x, batch_size=BATCH_SIZE,verbose=vb0)
+  for k,vs in track.items():
+    best_max = -float("inf")
+    for j in range(vs[0],vs[1]):
+      if best_max < outs[j]:
+        best_max = outs[j]
+        selected_x = x[j]
+        selected_y = y[j]
+    X_new.append(selected_x)
+    y_new.append(selected_y)
+    outs_new.append(best_max)
+  return np.array(X_new), np.array(y_new), np.array(outs_new)
+
+
+# load data from files on netmhc website
+# third column, binding strength
+# input: file
+# output: x_data matrix, y_data matrix, tracking info dictionary
+def encoding(data_x,data_y):
+    X, y = [], []
+    track = {}
+    for i,line in enumerate(data_x):
+        # track which original datapoint each window belongs to
+        begin, end = len(X), len(X)
+        for s in make_windows(data_x):
+            X.append(s)
+            y.append(data_y[i])
+            end += 1
+            # data in the range of (begin, end) belongs to datapoint i
+        track[i] = (begin, end)
+    return np.array(X), np.array(y), track
   
 #main function
-for _ in range(0,1):
+def main():
     #initiate 
     inputs=[]
     outputs=[]
@@ -219,13 +251,11 @@ for _ in range(0,1):
     class_set = set()
     max_len = 0
     X_train = []
-    X_train_p = []
-    X_train_n = []
-    X_val_p = []
-    X_val_n = []
+    y_val = []
+    X_val = []
     y_train = []
-    y_val_p = []
-    y_val_n = []
+    y_val = []
+    y_val_linear = []
     
     #file_name0 ='HLADRB10401simplev1_tr_1_val.csv'
     for line in open(path_data+file_name0,'r'):
@@ -234,21 +264,21 @@ for _ in range(0,1):
             raise Exception("Output should be single characer")
         else:
             if out_ == '0' :
-                X_train_n.append(in_)
                 X_train.append(in_)
-                y_train.append(out_)
+                y_train.append([1,0])
             elif out_ == '1':
-                X_train_p.append(in_)
                 X_train.append(in_)
-                y_train.append(out_)
+                y_train.append([0,1])
             else:
                 out_ = str(int(out_) -2)
                 if out_ == '0':
-                    X_val_n.append(in_)
-                    y_val_n.append(out_)
+                    X_val.append(in_)
+                    y_val.append([1,0])
+                    y_val_linear.append(0)
                 else:
-                    X_val_p.append(in_)
-                    y_val_p.append(out_)
+                    X_val.append(in_)
+                    y_val.append([0,1])
+                    y_val_linear.append(1)
               
             max_len = max([max_len,len(in_),len(out_)])
             inputs.append(in_)
@@ -256,38 +286,13 @@ for _ in range(0,1):
             
             #for c in in_: char_set.add(c)
             class_set.add(out_)
-    #
-    
-    #shuffle if indicated
-    if b_shuffle:
-        [X_train,y_train] = shuffle_train(X_train, y_train)
-        print('after shuffling, len(x)='+str(len(X_train)))      
-     
-    #creating encoding table
-    print(class_set)
-    classes = ''.join(class_set)
-    #ctable = CharacterTable(chars, MAXLEN)
-    #classtable = CharacterTable(classes, 1)
-    MAXLEN = max_len-2 #DIGITS + 1 + DIGITS
+        
 
     #create training or validation matrix
-    X_train_m = np.zeros((len(X_train), MAXLEN, len(chars)))
-    X_val_p_m = np.zeros((len(X_val_p), MAXLEN, len(chars)))
-    X_val_n_m = np.zeros((len(X_val_n), MAXLEN, len(chars)))
-    X_train_p_m = np.zeros((len(X_train_p), MAXLEN, len(chars)))
-    X_train_n_m = np.zeros((len(X_train_n), MAXLEN, len(chars)))
-    y_train_m = np.zeros((len(y_train), len(classes)))
-    y_val_p_m = np.zeros((len(y_val_p), len(classes)))
-    y_val_n_m = np.zeros((len(y_val_n), len(classes)))
-    
-    X_train = encoding(X_train_m, X_train,MAXLEN)
-    X_train_p = encoding(X_train_p_m, X_train_p,MAXLEN)
-    X_train_n = encoding(X_train_n_m, X_train_n,MAXLEN)
-    X_val_p = encoding(X_val_p_m, X_val_p,MAXLEN)
-    X_val_n = encoding(X_val_n_m, X_val_n,MAXLEN)
-    y_train = encoding(y_train_m, y_train,1)
-    y_val_p = encoding(y_val_p_m, y_val_p,1)
-    y_val_n = encoding(y_val_n_m, y_val_n,1)
+    X_train,y_train,track = encoding(X_train_m, y_train)
+    X_test,y_test,track_test = encoding(X_val, y_val)
+
+
     
     print(X_train[1,1,:])
     
@@ -295,104 +300,25 @@ for _ in range(0,1):
     y_val = np.concatenate((y_val_n,y_val_p))
     print('Training='+str(len(X_train))+' Validation='+str(len(X_val)))
     print("Input loaded ")
-    
 
-    
-    #Create checkpoint
-    #checkpointer = ModelCheckpoint(filepath=model_name+'.weight', verbose=1, save_best_only=True)
-    # Train the model each generation and show predictions against the validation dataset
-    '''
-    if os.path.isfile(path_save+performance_file_name+v1+out_name+'.csv'):     
-        file_out = open(path_save+performance_file_name+v1+out_name+'.csv','a')
-    else:
-        file_out = open(path_save+performance_file_name+v1+out_name+'.csv','w+')
-    '''
-    #iterations = []
-    f2_val_best = []
-    n_best = []
-    ptotal0 = len(X_train_p)
-    ntotal0 = len(X_train_n)
-    training_n = str(ptotal0+ntotal0)
+    #training
     for iteration in range(1, n_iteration):
         #iterations.append(str(iteration))
         print()
         print('-' * 50)
         print('Iteration', iteration)
 
-        
-        
-            
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        model.fit(X_train, y_train, batch_size=BATCH_SIZE, verbose=vb0, nb_epoch=nb0, class_weight={1:1,0:1.0/ratio_t/2})      
-        #####predicting training
-        ptotal0 = len(X_train_p)
-        print('p_training='+str(ptotal0))
-        ntotal0 = len(X_train_n)
-        #print('Train_Postive')
-        #print(model.predict_classes(X_val_p)) 
-        tp0 = sum(model.predict_classes(X_train_p,verbose=vb0))+0.1
-        #print('Train_Negative')
-        #print(model.predict_classes(X_val_n)) 
-        fp0 = sum(model.predict_classes(X_train_n,verbose=vb0))
-        tn0 = ntotal0 - fp0
-        fn0 = ptotal0 - tp0
-        train_pre = str(float(tp0)/(tp0+fp0))
-        train_recall = str(float(tp0)/(tp0+fn0))
-        train_f1 = calf1(train_pre, train_recall)
-        #print('Train_Precision='+str(float(tp0)/(tp0+fp0)))
-        #print('Train_Recall='+str(float(tp0)/(tp0+fn0)))
-        
-        ######predicting validation
-        #print('Val_Postive')
-        #print(model.predict_classes(X_val_p)) 
-        ptotal0 = len(X_val_p)
-        ntotal0 = len(X_val_n)
-        #predict
-        p_predicted = model.predict_classes(X_val_p,verbose=vb0)
-        #overall true positive
-        tp0 = sum(p_predicted)+0.1
-        #recall = tp/(total positive by gold standard)
-        #print('\n')
-        #print('X_train')
-        #print('p_predicted='+str(len(p_predicted)))
-        #print('mask_non_i='+str(len(mask_non_i)))
-        recall_non_i = sum(p_predicted[mask_non_i])/float(len_non_i)
-        recall_non_sub = sum(p_predicted[mask_non_sub])/float(len_non_sub)
-        #print('Val_Negative')
-        #print(model.predict_classes(X_val_n)) 
-        fp0 = sum(model.predict_classes(X_val_n,verbose=vb0))
-        tn0 = ntotal0 - fp0
-        fn0 = ptotal0 - tp0
-        val_pre=str(float(tp0)/(tp0+fp0))
-        val_recall=str(float(tp0)/(tp0+fn0))
-        val_f1 = calf1(val_pre,val_recall)
+        X_new, y_new, outs_new = select_best(X_train, y_train, track)
+        model.fit(X_new, y_train, batch_size=BATCH_SIZE, nb_epoch=nb,verbose=vb0)
+        _, y_test_select, test_outs = select_best(X_val, y_val, track_val)
+        auc_val = roc_auc_score(y_val_linear, test_outs)
         #print('Val_Precision='+str(float(tp0)/(tp0+fp0)))
         #print('Val_Recall='+str(float(tp0)/(tp0+fn0)))
         print('\n')
-        print('[iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub]')
-        print([iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub])
-        output_perf2([iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub])
-        #model.save_weights(path_save+file_name0+out_name+'_weight.h5',overwrite=True)
-    #save weights and performance info
-    #output_perf(file_out,file_name0,iterations,training_n, train_pre,train_recall,val_pre,val_recall)
-    #model.save_weights(path_save+file_name0+v1+'_weight.h5',overwrite=True)
-
-        
-#data_list is a list of lists with variable length
-data_list = []
-for long_pep in data_x:
-    data_list = ethan_magic(long_pep)
-        
-#create prediction for all elements
-update_step = 512
-
+        print([iteration,auc_val])
+        #print('[iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub]')
+        #print([iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub])
+        #output_perf2([iteration,train_pre,train_recall,train_f1,val_pre,val_recall,val_f1,recall_non_i,recall_non_sub])
+        model.save_weights(path_save+file_name0+out_name+'_weight.h5',overwrite=True)
+    
+main()
