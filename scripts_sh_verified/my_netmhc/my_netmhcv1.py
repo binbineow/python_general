0dc299352c47a15d5a8e15285f21559571e1708c
binbineow <binbineow@gmail.com>
2016-12-04 00:00:33 -0800

    new blousm50.dict

--- scripts_sh_verified/my_netmhc/my_netmhcv1.py
+++ scripts_sh_verified/my_netmhc/my_netmhcv1.py
@@ -294,7 +294,7 @@ def main():
         X_new, y_new, outs_new = select_best(X_train, y_train_encoded, track, model)
         print(len(X_new))
         model.fit(X_new, y_train, batch_size=BATCH_SIZE, nb_epoch=nb,verbose=vb0)
-        _, y_test_select, test_outs = select_best(X_val, y_val_encoded, track_val,model)
+        _, y_test_select, test_outs = select_best(X_val, y_val_encoded, track_tes,model)
         auc_val = roc_auc_score(y_val_linear, test_outs)
         #print('Val_Precision='+str(float(tp0)/(tp0+fp0)))
         #print('Val_Recall='+str(float(tp0)/(tp0+fn0)))
