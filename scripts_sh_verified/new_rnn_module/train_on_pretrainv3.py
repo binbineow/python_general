84f8b5dc2dfd99a257a9f193138a73228af32f6c
Binbin Chen <bchen45@Binbins-MacBook-Pro.local>
2017-05-11 00:55:13 -0700

    train_on_pretrain

--- scripts_sh_verified/new_rnn_module/train_on_pretrainv3.py
+++ scripts_sh_verified/new_rnn_module/train_on_pretrainv3.py
@@ -491,13 +491,13 @@ path_save = '/cstor/stanford/rbaltman/users/bchen45/mcl_data/model_weight/'
 rmsprop = keras.optimizers.RMSprop(lr=0.001)
 model_merge.compile(loss='categorical_crossentropy', optimizer=rmsprop)
 
-weight_list = make_w_list([80000,120000,80000],[x_train_pos[0],x_train_neg[0],x_train_iedb[0]])
+weight_list = make_w_list([80000,120000,50000],[x_train_pos[0],x_train_neg[0],x_train_iedb[0]])
 #weight_list = make_w_list([80000,100000],[x_train_pos[0],x_train_neg[0]])
 print(weight_list[0])
 print(weight_list[-1])
 #to save records
-weight_name = 'rnn_wopretrain_weightv5_d0.35.h5'
-record_file = path_save + 'record_trainingv5_d0.35.txt'
+weight_name = 'rnn_wopretrain_weightv5_d0.35.h5_8_12_5'
+record_file = path_save + 'record_trainingv5_d0.35.txt8_12_5'
 #parameters
 n_iteration = 20
 nb0 = 1
